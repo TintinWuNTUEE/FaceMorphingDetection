a7492d3ebba75a86139b5f40184285fa4a7acfdb
drowsyshooting <96434433+drowsyshooting@users.noreply.github.com>
2023-05-05 14:40:07 +0800

    kfold

--- rename.py
+++ rename.py
@@ -1,13 +1,14 @@
 import os
+
 i = 0
-path="./img_align_celeba"
+path = ".\data\img_align_celeba"
 for filename in os.listdir(path):
-	my_dest =filename.split('.')[0]+"_0"+ ".jpg"
-	print(my_dest)
-	my_source =os.path.join(path,filename)
-	print(my_source)
-	my_dest = os.path.join(path,my_dest)
-		# rename() function will
-		# rename all the files
-	os.rename(my_source, my_dest)
-	i += 1
+    my_dest = filename.split(".")[0] + "_0" + ".jpg"
+    print(my_dest)
+    my_source = os.path.join(path, filename)
+    print(my_source)
+    my_dest = os.path.join(path, my_dest)
+    # rename() function will
+    # rename all the files
+    os.rename(my_source, my_dest)
+    i += 1
